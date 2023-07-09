4b4c092332175de3fb4dd9891f04b4e1998e80f4
TheRad1 <18326@verona.k12.wi.us>
2023-07-09 03:04:14 -0500

    started on converting js to c++, stopped when it got to server things

--- Project-B-Server-C++/main.cpp
+++ Project-B-Server-C++/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
-int main() {
-	cout << "Howdy" << endl;
-	system("pause");
-}
+
+
+
+
+
+list<string> validCommands{"tu", "eu", "e", "newClient", "leave", "ping"};
+
+int currentID = 0;
+int eventTPS = 40;
+int transformTPS = 14;
+int SERVERPORT = 6969;
+int serverVersion = 3;
+
+const int checksBeforeDisconnect = 3;
+//setInterval(checkDisconnectTimers, 1000);
+
+list<string> playerTransformInfo{};
+list<string> playerInfo{};
+list<int> currentPlauerIDs{};
+list<int> playerDisconnectTimers{};
+list<string> eventsToSend{};
