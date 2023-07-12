c4184c44dfcde1f7dfe5fc63d69b1a21f7134a37
TheRad1 <18326@verona.k12.wi.us>
2023-07-12 16:42:44 -0400

    basics

--- Project-B-Server-C++/main.cpp
+++ Project-B-Server-C++/main.cpp
@@ -7,109 +7,104 @@
 #include <vector>
 
 using namespace std;
+
 #pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable:4996) 
+
 #define SERVER_PORT 6969
-#define BUFFER_SIZE 1024
-#define SERVER_VERSION 3
+#define BUFFER_LEN 1024
 const list<string> validCommands{"tu", "eu", "e", "newClient", "leave", "ping"};
 
 int currentID = 0;
-int eventTPS = 40;
-int transformTPS = 14;
+int currentUMessageID = 0;
 
-
+const int eventTPS = 40;
+const int transformTPS = 14;
+const int maxMessageID = 0;
 const int checksBeforeDisconnect = 3;
-//setInterval(checkDisconnectTimers, 1000);
+const int serverVersion = -1;
 
 list<string> playerTransformInfo{};
 list<string> playerInfo{};
 list<int> currentPlauerIDs{};
 list<int> playerDisconnectTimers{};
 list<string> eventsToSend{};
+list<int> uMessageID{};
 
+SOCKET server_socket;
+WSADATA wsa;
+sockaddr_in server, client;
 
-int main() {
-	// Initialize Winsock
-	WSADATA wsaData;
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		std::cerr << "Failed to initialize Winsock." << std::endl;
-		return 1;
+void initializeServer() 
+{
+	system("title UDP Server");
+
+	// initialise winsock
+	printf("Starting Server.\nInitialising Winsock...\n");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code: %d", WSAGetLastError());
+		exit(0);
 	}
+	printf("Initialised.\nCreating Socket...\n");
 
-	SOCKET server_socket;
-	struct sockaddr_in server_address, client_address;
-	char buffer[BUFFER_SIZE];
-
-	// Create server socket
-	server_socket = socket(AF_INET, SOCK_DGRAM, 0);
-	if (server_socket == INVALID_SOCKET) {
-		std::cerr << "Failed to create server socket." << std::endl;
-		WSACleanup();
-		return 1;
+	// create a socket
+	if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket: %d", WSAGetLastError());
 	}
+	printf("Socket created.\nSetting Bind Info...\n");
 
-	// Set server address
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(SERVER_PORT);
-	server_address.sin_addr.s_addr = INADDR_ANY;
+	// prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(SERVER_PORT);
+	printf("Info Setting Done.\Binding...\n");
 
-	// Bind server socket to server address
-	if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR) {
-		std::cerr << "Failed to bind server socket." << std::endl;
-		closesocket(server_socket);
-		WSACleanup();
-		return 1;
+	// bind
+	if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code: %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
 	}
+	puts("Bind done.\n");
+}
 
-	std::vector<SOCKET> client_sockets;  // Store client sockets
 
-	while (true) {
-		// Receive message from any client
-		struct sockaddr_in client_address;
-		int client_address_length = sizeof(client_address);
-		int received_bytes = recvfrom(server_socket, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&client_address, &client_address_length);
-		if (received_bytes == SOCKET_ERROR) {
-			std::cerr << "Failed to receive message." << std::endl;
-			closesocket(server_socket);
-			WSACleanup();
-			return 1;
+int main()
+{
+	initializeServer();
+
+	while (true)
+	{
+		printf("Waiting for data...");
+		fflush(stdout);
+		char message[BUFFER_LEN] = {};
+
+		// try to receive some data, this is a blocking call
+		int message_len;
+		int slen = sizeof(sockaddr_in);
+		if (message_len = recvfrom(server_socket, message, BUFFER_LEN, 0, (sockaddr*)&client, &slen) == SOCKET_ERROR)
+		{
+			printf("recvfrom() failed with error code: %d", WSAGetLastError());
+			exit(0);
 		}
 
-		// Print received message and client info
-		std::cout << "Received " << received_bytes << " bytes from " << inet_ntoa(client_address.sin_addr) << std::endl;
+		// print details of the client/peer and the data received
+		printf("Received packet from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+		printf("Data: %s\n", message);
 
-		// Add new client socket if not already in the list
-		bool new_client = true;
-		for (const SOCKET& client_socket : client_sockets) {
-			if (client_socket == server_socket)
-				continue;
-			if (client_address.sin_addr.s_addr == client_address.sin_addr.s_addr) {
-				new_client = false;
-				break;
-			}
-		}
-		if (new_client) {
-			client_sockets.push_back(server_socket);
-			std::cout << "New client connected: " << inet_ntoa(client_address.sin_addr) << std::endl;
-		}
+		cin.getline(message, BUFFER_LEN);
 
-		// Send response back to the client
-		if (sendto(server_socket, "pong", 4, 0, (struct sockaddr*)&client_address, client_address_length) == SOCKET_ERROR) {
-			std::cerr << "Failed to send response." << std::endl;
-			closesocket(server_socket);
-			WSACleanup();
-			return 1;
+		// reply the client with 2the same data
+		if (sendto(server_socket, message, strlen(message), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
+		{
+			printf("sendto() failed with error code: %d", WSAGetLastError());
+			return 3;
 		}
 	}
 
-	// Close client sockets
-	for (const SOCKET& client_socket : client_sockets) {
-		closesocket(client_socket);
-	}
-
-	// Close server socket
 	closesocket(server_socket);
 	WSACleanup();
-
 	return 0;
 }
